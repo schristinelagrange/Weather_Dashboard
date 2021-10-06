df90421c2d0a2e2d6ab8209cb9628ec2beaca27e
Christine Lagrange <schristinelagrange@gmail.com>
2021-10-06 14:40:21 -0400

    append list items

--- script.js
+++ script.js
@@ -2,19 +2,49 @@ var input = document.querySelector(".input");
 var output = document.querySelector(".output");
 var search = document.querySelector(".search");
 var weather = document.getElementById("container");
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API;
-var API = "a4b4716e88cadb45fae714953c38bd2c";
-var city = input.value;
 
 
-search.addEventListener("click", getWeather);
-output.textContent = localStorage.getItem("city");
+var cities = [];
 
-function getWeather() {
-	localStorage.setItem("city", input.value);
-	output.textContent = input.value;
-    fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data));
+function listCities() {
+    output.innerHTML="";
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+        var li = document.createElement("li");
+        li.textContent = city;
+        li.setAttribute("data-index", i);
+        output.appendChild(li);
+
+}};
+
+function save() {
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
+    if (savedCities !== null) {
+        cities = savedCities;
+    }
+    listCities();
 };
+
+function outputCities() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+search.addEventListener("click", function(event) {
+    event.preventDefault();
+    var cityInput = input.value.trim();
+    if (cityInput === "") {
+        return;
+    }
+    cities.push(cityInput);
+    input.value = "";
+
+    listCities();
+    outputCities();
+});
+
+save();
+
+
+
+
 
